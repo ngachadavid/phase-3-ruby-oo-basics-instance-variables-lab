5f01e28a3eee90ab2701ecc206c2539856c08aa6
ngachadavid <davidngacha01@gmail.com>
2023-02-09 17:03:56 +0300

    Done with assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        @this_dogs_name
+    end
+
+    # def name=(value)
+    #     @name=value
+    # end
+    # def name
+    #     @name
+    # end
 
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
+odie = Dog.new
+odie.name = "Odie"
+
+puts odie.name
